b7b8273a53d853aa51627defe59b8c3a3066c7f6
Valentin Khaydarov <valentin.khaydarov@gmail.com>
2022-02-13 19:00:19 +0100

    Adjustment to allow auto build and publishing

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 import setuptools
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
     name='MTPPy',
-    version='0.0.6',
+    version=get_version("src/mtppy/__init__.py"),
     url='',
     license='',
     author='Valentin Khaydarov, Tobias Kock',
     author_email='valentin.khaydarov@gmail.com',
     description='Open-source modular automation',
-    install_requires=[
-        'opcua~=0.98.13'
-    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
