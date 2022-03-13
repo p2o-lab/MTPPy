371f2c55779501e02e718d523ca81c80214ead9c
Valentin Khaydarov <valentin.khaydarov@gmail.com>
2022-03-13 02:55:46 +0100

    Examples added

--- examples/virtual_pea_with_recipe.py
+++ examples/virtual_pea_with_recipe.py
@@ -1,7 +1,9 @@
+from mtppy.opcua_server_pea import OPCUAServerPEA
 from mtppy.service import Service
 from mtppy.procedure import Procedure
 from mtppy.operation_elements import *
 from mtppy.indicator_elements import *
+
 import time
 import random
 
@@ -80,7 +82,8 @@ class ServiceDummy(Service):
 
             if self.procedure_control.get_procedure_cur() == 2:
                 self.procedures[2].report_values['proc_rv_ana'].set_v(random.random())
-                self.procedures[2].report_values['proc_rv_bin'].set_v(not self.procedures[2].report_values['proc_rv_bin'].attributes['V'].value)
+                self.procedures[2].report_values['proc_rv_bin'].set_v(
+                    not self.procedures[2].report_values['proc_rv_bin'].attributes['V'].value)
                 self.procedures[2].report_values['proc_rv_dint'].set_v(random.randint(-100, 100))
                 self.procedures[2].report_values['proc_rv_str'].set_v(str(random.random()))
 
@@ -126,3 +129,66 @@ class ServiceDummy(Service):
     def resetting(self):
         print('- Resetting -')
         self.state_change()
+
+
+if __name__ == '__main__':
+    module = OPCUAServerPEA()
+
+    # Service definition
+    service_1 = ServiceDummy('dummy', 'description')
+    module.add_service(service_1)
+
+    # Start server
+    print('--- Start OPC UA server ---')
+    module.run_opcua_server()
+
+    # Test
+    opcua_server = module.get_opcua_server()
+    opcua_ns = module.get_opcua_ns()
+    time.sleep(1)
+    print('--- Set parameters of service dummy to Operator mode ---')
+    opcua_server.get_node(
+        'ns=3;s=services.dummy.configuration_parameters.serv_param_ana.op_src_mode.StateOpOp').set_value(True)
+    opcua_server.get_node(
+        'ns=3;s=services.dummy.configuration_parameters.serv_param_dint.op_src_mode.StateOpOp').set_value(True)
+    opcua_server.get_node(
+        'ns=3;s=services.dummy.configuration_parameters.serv_param_bin.op_src_mode.StateOpOp').set_value(True)
+    opcua_server.get_node(
+        'ns=3;s=services.dummy.configuration_parameters.serv_param_str.op_src_mode.StateOpOp').set_value(True)
+
+    time.sleep(1)
+
+    print('--- Set parameters VOp of service dummy to different values ---')
+    opcua_server.get_node('ns=3;s=services.dummy.configuration_parameters.serv_param_ana.VOp').set_value(10.54)
+    opcua_server.get_node('ns=3;s=services.dummy.configuration_parameters.serv_param_dint.VOp').set_value(-5.11)
+    opcua_server.get_node('ns=3;s=services.dummy.configuration_parameters.serv_param_bin.VOp').set_value(True)
+    opcua_server.get_node('ns=3;s=services.dummy.configuration_parameters.serv_param_str.VOp').set_value('hello there')
+
+    time.sleep(1)
+
+    print('--- Change procedure to 2 ---')
+    opcua_server.get_node('ns=3;s=services.dummy.procedure_control.ProcedureOp').set_value(2)
+    time.sleep(1)
+
+    print('--- Set service dummy to Operator mode ---')
+    opcua_server.get_node('ns=3;s=services.dummy.op_src_mode.StateOpOp').set_value(True)
+    time.sleep(2)
+
+    print('--- Start service dummy ---')
+    opcua_server.get_node('ns=3;s=services.dummy.state_machine.CommandOp').set_value(4)
+    time.sleep(500)
+
+    print('--- Try to unhold service dummy ---')
+    opcua_server.get_node('ns=3;s=services.dummy.state_machine.CommandOp').set_value(32)
+    time.sleep(2)
+
+    print('--- Complete service dummy ---')
+    opcua_server.get_node('ns=3;s=services.dummy.state_machine.CommandOp').set_value(1024)
+    time.sleep(1)
+
+    print('--- Reset service dummy ---')
+    opcua_server.get_node('ns=3;s=services.dummy.state_machine.CommandOp').set_value(2)
+
+    print('--- Set service dummy to Offline mode ---')
+    opcua_server.get_node('ns=3;s=services.dummy.op_src_mode.StateOffOp').set_value(True)
+    time.sleep(1)
